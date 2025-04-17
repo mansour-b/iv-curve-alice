0f4859b4ff835281b855b05410ac3e449c9a26d2
Mansour Benbakoura <mansour.benbakoura@inria.fr>
2025-04-17 23:21:47 +0200

    plot r(t) relation

--- sweep_v_t_plot_iv.py
+++ sweep_v_t_plot_iv.py
@@ -21,17 +21,15 @@ def sweep_voltage(circuit):
     return voltages, currents
 
 
-def plot_iv_curve(ax, temperature, voltages, currents):
-    # Compute resistance through linear regression
+def measure_resistance(voltages, currents):
     result = linregress(x=currents, y=voltages)
-    resistance = result.slope
-    bias = result.intercept
-    resistance_std = result.stderr
-    bias_std = result.intercept_stderr
+    return result.slope
+
+
+def plot_iv_curve(ax, temperature, voltages, currents):
+    resistance = measure_resistance(voltages, currents)
 
     # Useful parameters for clean plot
-    abscissa = np.arange(-10, 10)
-
     min_v = min(voltages)
     max_v = max(voltages)
     amp_v = max_v - min_v
@@ -43,11 +41,7 @@ def plot_iv_curve(ax, temperature, voltages, currents):
     plot_y_limits = [min_c - 0.1 * amp_c, max_c + 0.1 * amp_c]
 
     # Plot the measurements
-    ax.errorbar(voltages, currents, xerr=0.1 * np.ones(len(voltages)), yerr=5e-4 * np.ones(len(currents)), fmt="-o", label=f"T = {temperature:.1f}K")
-
-    # # Plot the regression
-    # ax.plot(abscissa, (abscissa - bias) / resistance, zorder=0)
-    # plt.fill_between(abscissa, y1=(abscissa - bias + bias_std) / (resistance - resistance_std), y2=(abscissa - bias - bias_std) / (resistance + resistance_std), zorder=0, alpha=0.5, color="tab:orange")
+    ax.errorbar(voltages, currents, xerr=0.1 * np.ones(len(voltages)), yerr=5e-4 * np.ones(len(currents)), fmt="-o", label=f"T = {temperature:.0f} K, R = {resistance:.0f} Ohm")
 
     # Configure display
     ax.set_title("I-C curve of the thermistor")
@@ -58,17 +52,32 @@ def plot_iv_curve(ax, temperature, voltages, currents):
     ax.legend(loc="best")
 
 
+def plot_rt_curve(ax, temperatures, resistances):
+    ax.scatter(temperatures, resistances)
+    ax.set_xlabel("Temperature (K)")
+    ax.set_ylabel("Resistance (Ohm)")
+    ax.set_title("R(T) relation")
+
+
 if __name__ == "__main__":
     component = Thermistor()
     circuit = MeasuringCircuit(component)
     chamber = TemperatureControlledChamber(circuit)
 
-    fig, ax = plt.subplots(1, 1)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
 
+    temperatures = []
+    resistances = []
     for temperature in np.arange(270, 300, step=5):
         chamber.set_temperature(temperature)
         voltages, currents = sweep_voltage(circuit)
-        plot_iv_curve(ax, temperature, voltages, currents)
+
+        plot_iv_curve(ax1, temperature, voltages, currents)
+
+        temperatures.append(temperature)
+        resistances.append(measure_resistance(voltages, currents))
+
+    plot_rt_curve(ax2, temperatures, resistances)
 
     # Tell python to show the figure
     plt.show()
